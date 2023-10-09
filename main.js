9299da8051550346cc06a8fbf1bb9f474e6a09ea
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-10-09 16:15:19 -0300

    finishing project

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
 const input = document.querySelector("#input-text")
-const card = document.querySelector("#card-container")
-let count = 0
+const itemsContainer = document.querySelector("#items-container")
 
-input.addEventListener('keydown', function(event) {
+input.addEventListener('keydown', function (event) {
+
     if (event.keyCode === 13) {
-        
-        event.preventDefault()
-        
-        const element = createElement()
 
-        card.insertBefore(element, card.firstElementChild)
-        
+        event.preventDefault()
+
+        let text = input.value
+
+        if (text !== '') {
+
+            input.value = ''
+
+            if(text.includes(';')) {
+                const items = text.split(';')
+                
+                for(let item of items) {
+                    if(item !== '') {
+                        itemsContainer.appendChild(createItem(item))
+                    }
+                }
+            } else {
+                itemsContainer.appendChild(createItem(text))
+            }
+
+            itemsContainer.scrollTop = itemsContainer.scrollHeight
+        }
+
+
     }
 });
 
-function createElement() {
+function createItem(text) {
 
     const span = document.createElement('span')
-    span.classList.add('badge' ,'bg-success')
-    span.innerText = "click " + count++
+    span.classList.add('badge', 'bg-lime', 'me-2', 'mb-1')
+    // span.innerText = Math.floor(Math.random() * 1000000000)
+    span.innerText = text
+
+    const i = document.createElement('i')
+    i.classList.add('bi', 'bi-x', 'fs-6', 'ps-1')
+    i.addEventListener('click', (e) => {
+
+        e.preventDefault()
+        span.remove()
+
+    })
+
+    span.appendChild(i)
 
     return span
 }
